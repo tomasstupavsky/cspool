5f10e7b96b44200e0d3cfd178f2b48b663ef33ab
Tomas Stupavsky <tomass.stupavskyy@gmail.com>
2025-02-11 14:40:54 +0100

    added Ride and User interfaces

---
+++ web/lib/interfaces.ts
@@ -0,0 +1,20 @@
+import { Timestamp } from "@firebase/firestore";
+
+export interface Ride {
+  _id: number;
+  from: string;
+  to: string;
+  via: string;
+  price_euro: number;
+  passenger_ids: number[];
+  driver_id: number;
+  date: Timestamp;
+}
+
+export interface User {
+  _id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string
+}
