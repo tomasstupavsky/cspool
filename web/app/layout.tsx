fe58db32281a348b34758f3551f5cd7f02ffa5dd
Tomas Stupavsky <tomass.stupavskyy@gmail.com>
2025-02-12 10:46:55 +0100

    changed footer links

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -51,11 +51,11 @@ export default function RootLayout({
               <Link
                 isExternal
                 className="flex items-center gap-1 text-current"
-                href="https://heroui.com?utm_source=next-app-template"
-                title="heroui.com homepage"
+                href="https://tomasstupavsky.sk"
+                title="Stránka vývojára"
               >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">HeroUI</p>
+                <span className="text-default-600">&copy; 2025</span>
+                <p className="text-primary">Tomáš Stupavský</p>
               </Link>
             </footer>
           </div>
